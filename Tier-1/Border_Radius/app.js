b58ec8815002a238bebb11e94b807cad3202277b
Chugil.C <chugilrc13@gmail.com>
2022-01-06 21:47:21 +0530

    CSS Blur

---
+++ Tier-1/Border_Radius/app.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn");
+
+function calcRad() {
+  // Element values
+  const toL = +document.querySelector(".tl").value;
+  const toR = +document.querySelector(".tr").value;
+  const boL = +document.querySelector(".bl").value;
+  const boR = +document.querySelector(".br").value;
+}
+
+btn.addEventListener("click", calcRad);
